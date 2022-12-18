79026c02777450d4824c9c93ec18365e7b6a9f21
Sushmitacode12 <sushmitatripathi786@gmail.com>
2022-12-18 18:28:55 +0530

    date component added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = "Rs 4600";
-  const LocationofExpenditure = "Location of Expenditure";
-
+function ExpenseItem(props) {
+  
   return (
      <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
-        <div className="expense-item h2">{LocationofExpenditure}</div>
+      <ExpenseDetails amount ={props.amount} />
+        
       </div> 
     </div>
   );
-}
+  }
 
 export default ExpenseItem;
